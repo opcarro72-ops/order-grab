319735864fe4fc3750c32abe3cfd8051eb7209bd
opcarro72 <opcarro72@gmail.com>
2026-05-04 00:22:58 +0700

    Updated design and pages

--- public/login.js
+++ public/login.js
@@ -27,6 +27,39 @@ async function login() {
   }
 }
 
+function changeLang(lang){
+  localStorage.setItem("lang", lang);
+
+  if(lang === "bn"){
+    document.querySelector("h1").innerText = "হ্যালো, স্বাগতম!";
+    document.querySelector("h2").innerText = "লগইন";
+
+    document.getElementById("username").placeholder = "৬-১৬ অক্ষর বা সংখ্যা";
+    document.getElementById("password").placeholder = "৬-১৬ অক্ষরের পাসওয়ার্ড";
+
+    document.querySelector(".remember label") 
+      ? document.querySelector(".remember label").innerText = "পাসওয়ার্ড মনে রাখুন"
+      : document.querySelector(".remember").innerHTML = '<input type="checkbox" checked> পাসওয়ার্ড মনে রাখুন';
+
+    document.querySelector(".login-btn").innerText = "লগইন";
+    document.querySelector(".register").innerText = "রেজিস্ট্রেশন";
+
+  }else{
+    document.querySelector("h1").innerText = "Hello,Welcome!";
+    document.querySelector("h2").innerText = "Login";
+
+    document.getElementById("username").placeholder = "6-16 letters or numbers";
+    document.getElementById("password").placeholder = "6-16 alphanumeric password";
+
+    document.querySelector(".remember label") 
+      ? document.querySelector(".remember label").innerText = "Remember password"
+      : document.querySelector(".remember").innerHTML = '<input type="checkbox" checked> Remember password';
+
+    document.querySelector(".login-btn").innerText = "Login";
+    document.querySelector(".register").innerText = "Registration";
+  }
+}
+
 function goRegister() {
   window.location.href = "/register.html";
 }
